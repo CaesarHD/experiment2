2d53c20dfef345a5c0937eb11e47004d50e94d78
CaesarHD <stir.cezar@gmail.com>
2019-11-02 19:00:27 +0200

    wrong changes

--- experimentProject/main.cpp
+++ experimentProject/main.cpp
@@ -7,10 +7,10 @@ int main()
     float a,b,c,x1,x2,d,x;
 
     cout<<"a=";
-    cin>>a;
-    cout<<"b=";
-    cin>>b;
-    cout<<"c=";
+    cin>>c;
+    cout<<"b="
+    cin>>asd
+    cout<<"c=asd
     cin>>c;
     return 0;
 }
